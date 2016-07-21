6b7dee3be1dc0e59fa599986cf3d8c69f4e3490a
s-ichikawa <ichikawa.shingo.0829@gmail.com>
2016-07-22 06:41:51 +0900

    Eloquent練習用テーブル

---
+++ database/migrations/2016_07_21_212903_create_phones_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phones', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->text('number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('phones');
+    }
+}
