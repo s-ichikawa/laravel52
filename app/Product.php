19740ebb339c180ba410071421dc163ced28c443
s-ichikawa <ichikawa.shingo.0829@gmail.com>
2016-08-03 23:54:17 +0900

    ポリモーフィック関係

---
+++ app/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    public function photos()
+    {
+        return $this->morphMany(Photo::class, 'imageable');
+    }
+}
