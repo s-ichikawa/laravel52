f9eb88fc0f1698cf5e171ca8950d434d39135f44
shingo.ichikawa <shingo.ichikawa@inte.co.jp>
2016-09-29 16:42:15 +0900

    LineNotify

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -48,5 +48,5 @@ Route::get('guzzle', function () {
 Route::group(['prefix' => 'notify'], function () {
     Route::get('/', 'LineNotifyController@index');
     Route::get('/auth', 'LineNotifyController@redirectToProvider');
-
+    Route::post('/callback', 'LineNotifyController@handleProviderCallback');
 });
