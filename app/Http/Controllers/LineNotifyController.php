0b3fc8242e0121d830fd370be7d46d1f874b4805
shingo.ichikawa <shingo.ichikawa@inte.co.jp>
2016-09-29 16:26:15 +0900

    LineNotify

--- app/Http/Controllers/LineNotifyController.php
+++ app/Http/Controllers/LineNotifyController.php
@@ -2,16 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use Socialite;
+use URL;
 
 class LineNotifyController extends Controller
 {
+    public function index()
+    {
+        return view('line_notify.index');
+    }
+
     public function redirectToProvider()
     {
-        return Socialite::driver('line_notify')->redirect();
+        $uri = 'https://notify-bot.line.me/oauth/authorize?' .
+            'response_type=code' . '&' .
+            'client_id=' . config('services.line_notify.client_id') . '&' .
+            'redirect_uri=' . config('services.line_notify.redirect_uri') . '&' .
+            'scope=notify' . '&' .
+            'state=' . csrf_token() . '&' .
+            'response_mode=form_post';
+        return redirect($uri);
     }
 
     public function handleProviderCallback()
---
+++ resources/views/line_notify/index.blade.php
@@ -0,0 +1,2 @@
+<a href="/notify/auth">Line Notify Auth</a>
+
