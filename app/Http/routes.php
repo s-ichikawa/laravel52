d7b7cc4839cd7df1578b80991636d01b35a96ed4
s-ichikawa <ichikawa.shingo.0829@gmail.com>
2016-10-01 19:31:39 +0900

    SendGrid Sample

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -45,12 +45,12 @@ Route::get('guzzle', function () {
     return response("OK");
 });
 
-Route::group(['prefix' => 'notify'], function () {
-    Route::get('/', 'LineNotifyController@index');
-    Route::get('/auth', 'LineNotifyController@redirectToProvider');
-    Route::post('/callback', 'LineNotifyController@handleProviderCallback');
-    Route::post('/send', 'LineNotifyController@send');
-});
+//Route::group(['prefix' => 'notify'], function () {
+//    Route::get('/', 'LineNotifyController@index');
+//    Route::get('/auth', 'LineNotifyController@redirectToProvider');
+//    Route::post('/callback', 'LineNotifyController@handleProviderCallback');
+//    Route::post('/send', 'LineNotifyController@send');
+//});
 
 Route::post('/sendgrid/event', function (\Illuminate\Http\Request $request) {
     Log::info($request->all());
